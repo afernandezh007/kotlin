3b10982e4c9954882835bd1849afe3ae3a6381e4
Antonio Fernández <antonio.fernandez.hernandez@gmail.com>
2017-08-12 20:19:52 +0200

    Conditionals

--- src/conditionals/Conditionals.kt
+++ src/conditionals/Conditionals.kt
@@ -1,6 +1,14 @@
+package conditionals
+
 fun main(args: Array<String>) {
-    val age: Int = 21
-    if (age<18){
+    val age: Int = 19
+    if (age < 18) {
         println("You cannot register.")
+    } else if (age < 21) {
+        println("Well, maybe you can register.")
+    } else if (age < 27) {
+        println("Yo should be able to register.")
+    } else {
+        println("You're good to go!")
     }
 }
