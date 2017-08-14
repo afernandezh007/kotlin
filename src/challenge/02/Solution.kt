41d0891c7b5d9d26e591046d72943eb781999832
Antonio Fernández <antonio.fernandez.hernandez@gmail.com>
2017-08-14 14:29:03 +0200

    challenge 02 about loops

---
+++ src/challenge/02/Solution.kt
@@ -0,0 +1,15 @@
+package challenge.`02`
+
+/**
+ * Created by neome on 14/08/2017.
+ */
+
+fun main(args: Array<String>) {
+
+    var sum = 0L
+    for( i in 100L..100000L){
+        sum+=i
+    }
+    println(sum)
+
+}
