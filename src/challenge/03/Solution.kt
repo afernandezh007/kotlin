07db0a652d22b4d6fc06e1679a95205028f4c817
Antonio Fernández <antonio.fernandez.hernandez@gmail.com>
2017-08-14 14:40:08 +0200

    challenge 03 about loops and arrays

---
+++ src/challenge/03/Solution.kt
@@ -0,0 +1,21 @@
+package challenge.`03`
+
+/**
+ * Created by neome on 14/08/2017.
+ */
+
+fun main(args: Array<String>) {
+
+    var books = arrayListOf("ESDLA", "The Dark Tower", "2012", "Alice in Wonderland");
+
+    for (b in books) {
+        if (b.contains("e")) {
+            for (c in b) {
+                println(c)
+            }
+        }
+        println()
+    }
+
+
+}
