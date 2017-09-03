36ca27d1bbcbdad71a5b2f1e5113b872cdd4b94c
Antonio Fernández <antonio.fernandez.hernandez@gmail.com>
2017-09-03 22:41:23 +0200

    i/o example

---
+++ src/io/NumberGuessing.kt
@@ -0,0 +1,28 @@
+package io
+
+import java.util.*
+
+fun main(args: Array<String>) {
+    val number = Random().nextInt(100) + 1
+    var input: String?
+    var guess = -1
+
+    while (guess != number) {
+        print("Guess a number between 1 and 100: ")
+        input = readLine()
+
+        if (input != null) {
+            guess = input.toInt()
+        }
+
+        if (guess < number) {
+            println("too low")
+        } else if (guess > number) {
+            println("too large")
+        } else {
+            println("you won!")
+        }
+
+        println(guess)
+    }
+}
