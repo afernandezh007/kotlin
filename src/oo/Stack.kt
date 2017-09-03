ebe142373e70c9981f9993fd44a9749bbfd345e8
Antonio Fernández <antonio.fernandez.hernandez@gmail.com>
2017-09-03 22:26:10 +0200

    Generic stack

---
+++ src/oo/Stack.kt
@@ -0,0 +1,33 @@
+package oo
+
+class Stack<E>(vararg val items: E) {
+
+    val elements = items.toMutableList()
+
+    fun push(element: E) {
+        elements.add(element)
+    }
+
+    fun pop(): E? {
+        if(!isEmpty()) {
+            return elements.removeAt(elements.size - 1)
+        }
+        return null
+    }
+
+    fun isEmpty():Boolean{
+        return elements.isEmpty()
+    }
+}
+
+fun main(args: Array<String>) {
+    val stack = Stack(3,5,2,8)
+
+    stack.push(11)
+
+    println(stack.pop())
+    println(stack.pop())
+    println(stack.pop())
+    println(stack.pop())
+    println(stack.pop())
+}
