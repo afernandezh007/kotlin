c762924256e72c3fe9218cd6b7e550c725caa0d0
Antonio Fernández <antonio.fernandez.hernandez@gmail.com>
2017-09-03 14:54:18 +0200

    Override

---
+++ src/oo/Overriding.kt
@@ -0,0 +1,29 @@
+package oo
+
+/**
+ * Created by neome on 03/09/2017.
+ */
+abstract class Course(val topic: String, val price: Double) {
+    open fun learn() {
+        println("Learning a $topic course.")
+    }
+}
+
+interface Learnable {
+    fun learn() {
+        println("Learning...")
+    }
+}
+
+open class KotlinCourse() : Course("Kotlin", 999.99), Learnable {
+    override final fun learn() {
+        super<Learnable>.learn()
+        println("I'm one of the first people to learn Kotlin!")
+    }
+}
+
+
+fun main(args: Array<String>) {
+    val course = KotlinCourse()
+    course.learn()
+}
