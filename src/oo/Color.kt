4b28e5bbe4ee22002080c53d22a3ff507c1e1a0e
Antonio Fernández <antonio.fernandez.hernandez@gmail.com>
2017-09-03 21:22:51 +0200

    enum and binary operators

--- src/oo/Color.kt
+++ src/oo/Color.kt
@@ -1,5 +1,25 @@
 package oo
 
-enum class Color{
-    RED, GREEN, BLUE
+enum class Color(val rgb:Int){
+    RED(0xFF0000), GREEN(0x00FF00), BLUE(0x0000FF);
+
+    fun containsRed():Boolean{
+        return this.rgb and 0xFF0000 != 0
+    }
+}
+
+fun main(args: Array<String>) {
+
+    println(Color.RED.containsRed())
+    println(Color.GREEN.containsRed())
+    println(Color.BLUE.containsRed())
+
+    //bitwise operators
+
+    println(0b1101 or 0b1000) //13
+
+    println(0b1101 xor 0b1000) //5
+
+    println(0b10011.inv() and 0x00001F)
+
 }
