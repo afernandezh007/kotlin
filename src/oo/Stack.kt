c47e4cfc0a0b2bf4cd84320892b8f9e1984aaf16
Antonio Fernández <antonio.fernandez.hernandez@gmail.com>
2017-09-03 22:32:45 +0200

    Generics function

--- src/oo/Stack.kt
+++ src/oo/Stack.kt
@@ -9,21 +9,32 @@ class Stack<E>(vararg val items: E) {
     }
 
     fun pop(): E? {
-        if(!isEmpty()) {
+        if (!isEmpty()) {
             return elements.removeAt(elements.size - 1)
         }
         return null
     }
 
-    fun isEmpty():Boolean{
+    fun isEmpty(): Boolean {
         return elements.isEmpty()
     }
 }
 
+fun <T> stackOf(vararg elements: T): Stack<T> {
+    return Stack(*elements)
+}
+
+
 fun main(args: Array<String>) {
-    val stack = Stack(3,5,2,8)
+    val stack = Stack(3, 5, 2, 8)
 
     stack.push(11)
+
+    val stack2 = stackOf("Hi", "Hey", "Hello")
+
+    for(i in 1..3){
+        println(stack2.pop())
+    }
 
     println(stack.pop())
     println(stack.pop())
